1a0a94383e164b140d474318f2067fb6f5edaaf0
ahianzhang <zhangxinshui@shopin.cn>
2020-07-20 13:25:45 +0800

    feat: initRepo

---
+++ src/main/java/com/ahianzhang/amqp/client/AmqpClientApplication.java
@@ -0,0 +1,13 @@
+package com.ahianzhang.amqp.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmqpClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmqpClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ahianzhang/amqp/client/AmqpClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahianzhang.amqp.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmqpClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
